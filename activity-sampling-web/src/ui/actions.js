adf2e93de4633061c1abb9dc0ec329a3615263e4
Falko Schumann <falko.schumann@muspellheim.de>
2023-10-09 09:48:58 +0200

    refactor: use currying
    to simplify components

---
+++ activity-sampling-web/src/ui/actions.js
@@ -0,0 +1,33 @@
+import { getRecentActivities } from '../application/services.js';
+import { FakeRepository } from '../infrastructure/repository.js';
+
+let repository = new FakeRepository([
+  {
+    timestamp: new Date('2023-10-07T13:00:00Z'),
+    duration: 'PT30M',
+    client: 'Muspellheim',
+    project: 'Activity Sampling',
+    task: 'Recent Activities',
+    notes: 'Show my recent activities',
+  },
+  {
+    timestamp: new Date('2023-10-07T12:30:00Z'),
+    duration: 'PT30M',
+    client: 'Muspellheim',
+    project: 'Activity Sampling',
+    task: 'Recent Activities',
+    notes: 'Show my recent activities',
+  },
+  {
+    timestamp: new Date('2023-10-07T12:00:00Z'),
+    duration: 'PT30M',
+    client: 'Muspellheim',
+    project: 'Activity Sampling',
+    task: 'Recent Activities',
+    notes: 'Show my recent activities',
+  },
+]);
+
+export async function getRecentActivities() {
+  return await getRecentActivities(repository);
+}
--- activity-sampling-web/src/ui/components.js
+++ activity-sampling-web/src/ui/components.js
@@ -1,38 +1,10 @@
 import { html, render } from 'lit-html';
-import { getRecentActivities } from '../application/services.js';
 
-import { FakeRepository } from '../infrastructure/repository.js';
-
-let repository = new FakeRepository([
-  {
-    timestamp: new Date('2023-10-07T13:00:00Z'),
-    duration: 'PT30M',
-    client: 'Muspellheim',
-    project: 'Activity Sampling',
-    task: 'Recent Activities',
-    notes: 'Show my recent activities',
-  },
-  {
-    timestamp: new Date('2023-10-07T12:30:00Z'),
-    duration: 'PT30M',
-    client: 'Muspellheim',
-    project: 'Activity Sampling',
-    task: 'Recent Activities',
-    notes: 'Show my recent activities',
-  },
-  {
-    timestamp: new Date('2023-10-07T12:00:00Z'),
-    duration: 'PT30M',
-    client: 'Muspellheim',
-    project: 'Activity Sampling',
-    task: 'Recent Activities',
-    notes: 'Show my recent activities',
-  },
-]);
+import { getRecentActivities } from './actions.js';
 
 class RecentActivities extends HTMLElement {
   async connectedCallback() {
-    let recentActivities = await getRecentActivities(repository);
+    let recentActivities = await getRecentActivities();
     let template = html`
       <section class="recent-activities">
         ${recentActivities.workingDays.map(({ date, activities }) =>
