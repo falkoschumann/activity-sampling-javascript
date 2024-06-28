d49dc952e6d337563dbbd6fbdd56d3218ef5348b
Falko Schumann <falko.schumann@muspellheim.de>
2024-06-28 18:02:09 +0200

    test: use messages

--- packages/webclient/test/integration/api.test.js
+++ packages/webclient/test/integration/api.test.js
@@ -1,7 +1,6 @@
 import { describe, expect, test } from '@jest/globals';
 
-import { Duration } from '@activity-sampling/shared';
-import { Activity } from '@activity-sampling/domain';
+import { Activity, RecentActivities } from '@activity-sampling/domain';
 import { Api } from '../../src/infrastructure/api.js';
 
 describe('API', () => {
@@ -16,49 +15,11 @@ describe('API', () => {
 
   test('Loads recent activities', async () => {
     const api = Api.createNull({
-      response: {
-        body: {
-          workingDays: [
-            {
-              date: new Date('2023-10-07T00:00Z'),
-              activities: [
-                Activity.createTestInstance({ timestamp: '2024-03-07T18:00Z' }),
-                Activity.createTestInstance({ timestamp: '2024-03-07T17:30Z' }),
-              ],
-            },
-          ],
-          timeSummary: {
-            hoursToday: 'PT1H',
-            hoursYesterday: 'PT0S',
-            hoursThisWeek: 'PT1H',
-            hoursThisMonth: 'PT1H',
-          },
-        },
-      },
+      response: { body: RecentActivities.createTestInstance() },
     });
 
     const activities = await api.selectRecentActivities();
 
-    expect(activities).toEqual({
-      workingDays: [
-        {
-          date: new Date('2023-10-07T00:00Z'),
-          activities: [
-            Activity.createTestInstance({
-              timestamp: new Date('2024-03-07T18:00Z'),
-            }),
-            Activity.createTestInstance({
-              timestamp: new Date('2024-03-07T17:30Z'),
-            }),
-          ],
-        },
-      ],
-      timeSummary: {
-        hoursToday: new Duration('PT1H'),
-        hoursYesterday: new Duration(),
-        hoursThisWeek: new Duration('PT1H'),
-        hoursThisMonth: new Duration('PT1H'),
-      },
-    });
+    expect(activities).toEqual(RecentActivities.createTestInstance());
   });
 });
