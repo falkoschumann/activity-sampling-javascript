e1fbdbd7a98903f7dc7819a7a9c2d534d2f10207
Falko Schumann <falko.schumann@muspellheim.de>
2024-07-29 20:27:28 +0200

    chore: remove duplicate controls

--- packages/webclient/src/ui/countdown.js
+++ packages/webclient/src/ui/countdown.js
@@ -1,15 +1,8 @@
 import { html } from 'lit-html';
-import { classMap } from 'lit-html/directives/class-map.js';
-import { createRef, ref } from 'lit-html/directives/ref.js';
 
-import { Duration } from '@activity-sampling/utils';
 import { Container } from '@activity-sampling/utils/src/browser.js';
 
-import { Services } from '../application/services.js';
-
 export class CountdownComponent extends Container {
-  #periodRef = createRef();
-
   constructor() {
     super();
     this.classList.add('h-stack', 'items-center', 'gap-50');
@@ -21,48 +14,12 @@ export class CountdownComponent extends Container {
 
   getView() {
     const progress = 1 - this.state.remainingTime / this.state.period;
-    const title = this.state.isRunning ? 'Stop countdown' : 'Start countdown';
     return html`
       <div class="expanded v-stack items-center gap-50">
-        <span>${this.state.remainingTime}</span>
+        <span class="caption">${this.state.remainingTime}</span>
         <progress class="full-width" max="1" value=${progress}></progress>
       </div>
-      <button
-        class="icon-only switch ${classMap({ on: this.state.isRunning })}"
-        title=${title}
-        aria-label=${title}
-        @click=${() => this.#toggle()}
-      >
-        <span class="material-icons">punch_clock</span>
-      </button>
-      <div class="select-wrapper icon-only">
-        <select
-          ${ref(this.#periodRef)}
-          name="period"
-          value="30"
-          title="Set period"
-        >
-          <optgroup label="Period">
-            <option value="5">5 minutes</option>
-            <option value="10">10 minutes</option>
-            <option value="15">15 minutes</option>
-            <option value="20">20 minutes</option>
-            <option value="30" selected>30 minutes</option>
-            <option value="60">60 minutes</option>
-            <option value="1">1 minute</option>
-          </optgroup>
-        </select>
-      </div>
     `;
-  }
-
-  async #toggle() {
-    if (this.state.isRunning) {
-      await Services.get().stopAskingPeriodically();
-    } else {
-      const period = new Duration(`PT${this.#periodRef.value.value}M`);
-      await Services.get().askPeriodically({ period });
-    }
   }
 }
 
