e8d6051c325e52c09fea022ac3fd41c63950acd1
Falko Schumann <falko.schumann@muspellheim.de>
2024-08-18 16:51:39 +0200

    chore: improve naming

--- packages/desktop/forge.config.js
+++ packages/desktop/forge.config.js
@@ -4,6 +4,8 @@ const { FuseV1Options, FuseVersion } = require('@electron/fuses');
 module.exports = {
   packagerConfig: {
     asar: true,
+    name: 'Activity Sampling',
+    executableName: 'activity-sampling',
   },
   rebuildConfig: {},
   makers: [
