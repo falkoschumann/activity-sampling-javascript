9086e5b664a74f0debaaf4c4b0ad23503668388f
Falko Schumann <falko.schumann@muspellheim.de>
2024-08-18 16:58:48 +0200

    fix: crash on startup

--- packages/desktop/src/main.js
+++ packages/desktop/src/main.js
@@ -3,11 +3,6 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('node:path');
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) {
-  app.quit();
-}
-
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
