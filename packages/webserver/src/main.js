e15232fd121f4e9b004179033e891aa972c7d591
Falko Schumann <falko.schumann@muspellheim.de>
2024-08-20 13:46:41 +0200

    docs: add todo

--- packages/webserver/src/main.js
+++ packages/webserver/src/main.js
@@ -1,5 +1,7 @@
 import { Application } from './ui/application.js';
 
+// TODO Configure repositpory's filename
+
 const port = process.env.PORT;
 const app = Application.create();
 app.start({ port });
