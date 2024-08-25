b1b8f3fc2f482030851430c0feb61d2550631417
Falko Schumann <falko.schumann@muspellheim.de>
2024-08-25 15:59:39 +0200

    test: add missing tests

--- packages/utils/test/ensuring.test.js
+++ packages/utils/test/ensuring.test.js
@@ -65,6 +65,12 @@ describe('Validation', () => {
         });
 
         test('Fails when value is not the expected type', () => {
+          expect(() => ensureType('a', undefined)).toThrow(
+            /The value must be undefined, but it was a string\./,
+          );
+        });
+
+        test('Fails when value is null', () => {
           expect(() => ensureType(null, undefined)).toThrow(
             /The value must be undefined, but it was null\./,
           );
@@ -79,6 +85,12 @@ describe('Validation', () => {
         });
 
         test('Fails when value is not the expected type', () => {
+          expect(() => ensureType(1, null)).toThrow(
+            /The value must be null, but it was a number\./,
+          );
+        });
+
+        test('Fails when value is undefined', () => {
           expect(() => ensureType(undefined, null)).toThrow(
             /The value must be null, but it was undefined\./,
           );
@@ -97,6 +109,18 @@ describe('Validation', () => {
             /The value must be a boolean, but it was a number\./,
           );
         });
+
+        test('Fails when value is undefined', () => {
+          expect(() => ensureType(undefined, Boolean)).toThrow(
+            /The value must be a boolean, but it was undefined\./,
+          );
+        });
+
+        test('Fails when value is null', () => {
+          expect(() => ensureType(null, Boolean)).toThrow(
+            /The value must be a boolean, but it was null\./,
+          );
+        });
       });
 
       describe('Number', () => {
@@ -111,6 +135,50 @@ describe('Validation', () => {
             /The value must be a number, but it was a string\./,
           );
         });
+
+        test('Fails when value is not a number', () => {
+          expect(() => ensureType(NaN, Number)).toThrow(
+            /The value must be a number, but it was NaN\./,
+          );
+        });
+
+        test('Fails when value is undefined', () => {
+          expect(() => ensureType(undefined, Number)).toThrow(
+            /The value must be a number, but it was undefined\./,
+          );
+        });
+
+        test('Fails when value is null', () => {
+          expect(() => ensureType(null, Number)).toThrow(
+            /The value must be a number, but it was null\./,
+          );
+        });
+      });
+
+      describe('BigInt', () => {
+        test('Returns value when it is the expected type', () => {
+          const result = ensureType(BigInt(42), BigInt);
+
+          expect(result).toBe(BigInt(42));
+        });
+
+        test('Fails when value is not the expected type', () => {
+          expect(() => ensureType(42, BigInt)).toThrow(
+            /The value must be a bigint, but it was a number\./,
+          );
+        });
+
+        test('Fails when value is undefined', () => {
+          expect(() => ensureType(undefined, BigInt)).toThrow(
+            /The value must be a bigint, but it was undefined\./,
+          );
+        });
+
+        test('Fails when value is null', () => {
+          expect(() => ensureType(null, BigInt)).toThrow(
+            /The value must be a bigint, but it was null\./,
+          );
+        });
       });
 
       describe('String', () => {
@@ -123,6 +191,72 @@ describe('Validation', () => {
         test('Fails when value is not the expected type', () => {
           expect(() => ensureType(123, String)).toThrow(
             /The value must be a string, but it was a number\./,
+          );
+        });
+
+        test('Fails when value is undefined', () => {
+          expect(() => ensureType(undefined, String)).toThrow(
+            /The value must be a string, but it was undefined\./,
+          );
+        });
+
+        test('Fails when value is null', () => {
+          expect(() => ensureType(null, String)).toThrow(
+            /The value must be a string, but it was null\./,
+          );
+        });
+      });
+
+      describe('Symbol', () => {
+        test('Returns value when it is the expected type', () => {
+          const symbol = Symbol();
+          const result = ensureType(symbol, Symbol);
+
+          expect(result).toBe(symbol);
+        });
+
+        test('Fails when value is not the expected type', () => {
+          expect(() => ensureType('John', Symbol)).toThrow(
+            /The value must be a symbol, but it was a string\./,
+          );
+        });
+
+        test('Fails when value is undefined', () => {
+          expect(() => ensureType(undefined, Symbol)).toThrow(
+            /The value must be a symbol, but it was undefined\./,
+          );
+        });
+
+        test('Fails when value is null', () => {
+          expect(() => ensureType(null, Symbol)).toThrow(
+            /The value must be a symbol, but it was null\./,
+          );
+        });
+      });
+
+      describe('Function', () => {
+        test('Returns value when it is the expected type', () => {
+          const func = () => {};
+          const result = ensureType(func, Function);
+
+          expect(result).toBe(func);
+        });
+
+        test('Fails when value is not the expected type', () => {
+          expect(() => ensureType('John', Function)).toThrow(
+            /The value must be a function, but it was a string\./,
+          );
+        });
+
+        test('Fails when value is undefined', () => {
+          expect(() => ensureType(undefined, Function)).toThrow(
+            /The value must be a function, but it was undefined\./,
+          );
+        });
+
+        test('Fails when value is null', () => {
+          expect(() => ensureType(null, Function)).toThrow(
+            /The value must be a function, but it was null\./,
           );
         });
       });
@@ -140,34 +274,58 @@ describe('Validation', () => {
             /The value must be an object, but it was a string\./,
           );
         });
-      });
-    });
 
-    describe('Array', () => {
-      test('Returns value when it is the expected type', () => {
-        const array = ['John', 'Smith'];
-        const result = ensureType(array, Array);
+        test('Fails when value is undefined', () => {
+          expect(() => ensureType(undefined, Object)).toThrow(
+            /The value must be an object, but it was undefined\./,
+          );
+        });
 
-        expect(result).toBe(array);
-      });
-
-      test('Fails when value is not the expected type', () => {
-        expect(() => ensureType('John', Array)).toThrow(
-          /The value must be an array, but it was a string\./,
-        );
+        test('Fails when value is null', () => {
+          expect(() => ensureType(null, Object)).toThrow(
+            /The value must be an object, but it was null\./,
+          );
+        });
       });
 
-      test('Returns value when items has the expected type', () => {
-        const names = ['John', 'Jane'];
-        const result = ensureType(names, [String]);
+      describe('Array', () => {
+        test('Returns value when it is the expected type', () => {
+          const array = ['John', 'Smith'];
+          const result = ensureType(array, Array);
 
-        expect(result).toBe(names);
-      });
+          expect(result).toBe(array);
+        });
 
-      test('Fails when items does not have the expected type', () => {
-        expect(() =>
-          ensureType(['John', 123], [String], { name: 'names' }),
-        ).toThrow(/The names.1 must be a string, but it was a number\./);
+        test('Fails when value is not the expected type', () => {
+          expect(() => ensureType('John', Array)).toThrow(
+            /The value must be an array, but it was a string\./,
+          );
+        });
+
+        test('Fails when value is undefined', () => {
+          expect(() => ensureType(undefined, Array)).toThrow(
+            /The value must be an array, but it was undefined\./,
+          );
+        });
+
+        test('Fails when value is null', () => {
+          expect(() => ensureType(null, Array)).toThrow(
+            /The value must be an array, but it was null\./,
+          );
+        });
+
+        test('Returns value when items has the expected type', () => {
+          const names = ['John', 'Jane'];
+          const result = ensureType(names, [String]);
+
+          expect(result).toBe(names);
+        });
+
+        test('Fails when items does not have the expected type', () => {
+          expect(() =>
+            ensureType(['John', 123], [String], { name: 'names' }),
+          ).toThrow(/The names.1 must be a string, but it was a number\./);
+        });
       });
     });
 
@@ -208,6 +366,22 @@ describe('Validation', () => {
           ),
         ).toThrow(/The User.age must be a number, but it was a string\./);
       });
+
+      test('Fails when value is undefined', () => {
+        expect(() =>
+          ensureType(
+            undefined,
+            { name: String, age: Number },
+            { name: 'User' },
+          ),
+        ).toThrow(/The User is required, but it was undefined\./);
+      });
+
+      test('Fails when value is null', () => {
+        expect(() =>
+          ensureType(null, { name: String, age: Number }, { name: 'User' }),
+        ).toThrow(/The User is required, but it was null\./);
+      });
     });
 
     describe('Constructor types', () => {
@@ -225,15 +399,21 @@ describe('Validation', () => {
         expect(result).toEqual(new Date(dateString));
       });
 
+      test('Fails when it can not convert to the expected type', () => {
+        expect(() => ensureType('no-date', Date)).toThrow(
+          /The value must be a valid Date, but it was a string: "no-date"\./,
+        );
+      });
+
       test('Fails when it it is undefined', () => {
         expect(() => ensureType(undefined, Date)).toThrow(
           /The value is required, but it was undefined\./,
         );
       });
 
-      test('Fails when it can not convert to the expected type', () => {
-        expect(() => ensureType('no-date', Date)).toThrow(
-          /The value must be a valid Date, but it was a string: "no-date"\./,
+      test('Fails when it it is null', () => {
+        expect(() => ensureType(null, Date)).toThrow(
+          /The value is required, but it was null\./,
         );
       });
     });
@@ -255,6 +435,18 @@ describe('Validation', () => {
       test('Fails when it can not convert to the expected type', () => {
         expect(() => ensureType('no-enum-constant', YesNo)).toThrow(
           /The value must be a YesNo, but it was a string\./,
+        );
+      });
+
+      test('Fails when it it is undefined', () => {
+        expect(() => ensureType(undefined, YesNo)).toThrow(
+          /The value is required, but it was undefined\./,
+        );
+      });
+
+      test('Fails when it it is null', () => {
+        expect(() => ensureType(null, YesNo)).toThrow(
+          /The value is required, but it was null\./,
         );
       });
     });
