5b982f441898b84560a93fc5a04afc4181481ed8
Falko Schumann <falko.schumann@muspellheim.de>
2024-09-09 17:31:38 +0200

    build: fix watch ignore pattern

--- packages/backend/jest.config.js
+++ packages/backend/jest.config.js
@@ -1,7 +1,7 @@
 /** @type {import('jest').Config} */
 const config = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup-files-after-env.js'],
-  watchPathIgnorePatterns: ['<rootDir>/packages/*/data/'],
+  watchPathIgnorePatterns: ['<rootDir>/data/'],
 };
 
 export default config;
