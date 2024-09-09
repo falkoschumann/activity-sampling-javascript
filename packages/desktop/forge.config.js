4baa992da275e5949a93083b19f76fb8e525d4f2
Falko Schumann <falko.schumann@muspellheim.de>
2024-09-09 21:32:41 +0200

    ci: disable asar validation

--- packages/desktop/forge.config.js
+++ packages/desktop/forge.config.js
@@ -57,7 +57,7 @@ module.exports = {
       [FuseV1Options.EnableCookieEncryption]: true,
       [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
       [FuseV1Options.EnableNodeCliInspectArguments]: false,
-      [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
+      //[FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
       [FuseV1Options.OnlyLoadAppFromAsar]: true,
     }),
   ],
